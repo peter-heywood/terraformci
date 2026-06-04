9ec3c237d563044a24f90ef54161dac6b717dfa3
peter-heywood <peter.heywood@homeoffice.gov.uk>
2026-06-04 16:02:23 +0100

    Added sqs queue

--- sqs.tf
+++ sqs.tf
@@ -3,7 +3,7 @@ resource "aws_sqs_queue" "sqs_message_queue" {
   max_message_size = 262144
 }
 
-resource "aws_sqs_queue" "sqs_message_queue_outbound" {
+resource "aws_sqs_queue" "sqs_message_queue_2" {
   name             = "outbound_sqs_queue"
   max_message_size = 262144
 }
