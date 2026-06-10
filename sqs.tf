af293acb07f933d21f24d2a73a863781644fb400
peter-heywood <peter.heywood@homeoffice.gov.uk>
2026-06-10 15:45:21 +0100

    Stage SQS queue added

--- sqs.tf
+++ sqs.tf
@@ -2,3 +2,8 @@ resource "aws_sqs_queue" "sqs_message_queue" {
   name             = "inbound_sqs_queue"
   max_message_size = 262144
 }
+
+resource "aws_sqs_queue" "sqs_message_queue" {
+  name             = "outbound_sqs_queue"
+  max_message_size = 262144
+}
